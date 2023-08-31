82937905e9778ce145762124ec68a24d786d9fbb
zbc951 <71483741+zbc951@users.noreply.github.com>
2023-08-31 13:10:07 +0800

    112-0831-1-N

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,5 @@
 $(function(){
-    $("aside .colorGroup").click(function(){
+    $("aside .colorGroup button").click(function(){
         $("aside .colorGroup ul li").toggleClass("display");
     })
 })
@@ -8,25 +8,59 @@ $(function(){
     $("aside .colorGroup ul li").click(function(){
         if($(this).hasClass("gold")){
             $(".colorSystem")
-            .attr("href","./css_colorStyle_gold/colorStyleGold.css");
+            .attr("href","./css_colorStyle_gold/colorStyle.css");
         } else if($(this).hasClass("claret")){
             $(".colorSystem")
-            .attr("href","./css_colorStyle_claret/colorStyleClaret.css");
+            .attr("href","./css_colorStyle_claret/colorStyle.css");
         } else if($(this).hasClass("gray")){
             $(".colorSystem")
-            .attr("href","./css_colorStyle_gray/colorStyleGray.css");
+            .attr("href","./css_colorStyle_gray/colorStyle.css");
         } else if($(this).hasClass("green")){
             $(".colorSystem")
-            .attr("href","./css_colorStyle_green/colorStyleGreen.css");
+            .attr("href","./css_colorStyle_green/colorStyle.css");
         } else if($(this).hasClass("pink")){
             $(".colorSystem")
-            .attr("href","./css_colorStyle_pink/colorStylePink.css");
+            .attr("href","./css_colorStyle_pink/colorStyle.css");
         } else if($(this).hasClass("red")){
             $(".colorSystem")
-            .attr("href","./css_colorStyle_red/colorStyleRed.css");
+            .attr("href","./css_colorStyle_red/colorStyle.css");
         } else if($(this).hasClass("blue")){
             $(".colorSystem")
             .attr("href","./css_colorStyle/colorStyle.css");
+        } else if($(this).hasClass("purple-gray")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_purple-gray/colorStyle.css");
+        }else if($(this).hasClass("purple-red")){
+                $(".colorSystem")
+                .attr("href","./css_colorStyle_purple-red/colorStyle.css");
+        } else if($(this).hasClass("yellow-blue")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_yellow-blue/colorStyle.css");
+        } else if($(this).hasClass("purple-blue")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_purple-blue/colorStyle.css");
+        } else if($(this).hasClass("purple")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_purple/colorStyle.css");
+        }  else if($(this).hasClass("orange-gray")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_orange-gray/colorStyle.css");
+        }  else if($(this).hasClass("orange-blue")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_orange-blue/colorStyle.css");
+        }  else if($(this).hasClass("blue-skin")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_blue-skin/colorStyle.css");
+        }  else if($(this).hasClass("blue-gold")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_blue-gold/colorStyle.css");
+        }   else if($(this).hasClass("orange-gold")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_orange-gold/colorStyle.css");
+        }   else if($(this).hasClass("orange-green")){
+            $(".colorSystem")
+            .attr("href","./css_colorStyle_orange-green/colorStyle.css");
         } 
+         
     })
 })
